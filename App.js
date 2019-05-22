c4cd8ef67d44b8ec6a80b8a4e03002aa36ad0780
Mathias Kofoed <mrk@cmp-company.com>
2019-05-22 23:30:06 +0200

    Add and cache font assets

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import { useScreens } from "react-native-screens";
+import { AppLoading, Font } from "expo";
+import { Feather } from "@expo/vector-icons";
 import Router from "./src/Router";
+import Cubano from "./assets/cubano-regular.otf";
+import Lato from "./assets/lato-regular.ttf";
 
 useScreens();
 
@@ -9,7 +13,25 @@ export default class App extends Component {
     assetsCached: false,
   }
 
+  async cacheStaticAssets() {
+    const cachedFonts = [
+      { Cubano },
+      { Lato },
+      Feather.font,
+    ].map(font => Font.loadAsync(font));
+    await Promise.all(cachedFonts);
+  }
+
   render() {
+    const { assetsCached } = this.state;
+    if (!assetsCached) {
+      return (
+        <AppLoading
+          startAsync={ this.cacheStaticAssets }
+          onFinish={ () => this.setState({ assetsCached: true }) }
+        />
+      );
+    }
     return (
       <Router />
     );
